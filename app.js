ae5f1a451e23b3eb5b2798116ce3c614a87620cf
Osinachi Chibuzor <70204435+Chibuzor-byte@users.noreply.github.com>
2025-08-24 21:58:25 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// GradeBook APP - task from Freecodecamp
+
+//STEP 1
+
+function getAverage(scores) {
+    let sum = 0;
+    for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+    
+  }
+  let total = scores.length;
+  let average = sum/total;
+  return average
+ 
+}
+
+
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
